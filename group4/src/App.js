1bd4504fa4839e729b9bfb2bd12cb2070c7936e7
Vidhya E N <envidhya99@gmail.com>
2023-05-02 20:35:17 +0530

    Updated the viewchanges

--- group4/src/App.js
+++ group4/src/App.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import React from 'react';
 import Footer from './component/footer';
@@ -74,80 +75,49 @@ export default App;
 /*import React from 'react';
 import Footer from './component/footer';
 import Header from './component/header';
+=======
+import React from "react";
+import Footer from "./components/footer";
+import Header from "./components/header";
+import { Routes, Route } from "react-router-dom";
+import Certifications from "./components/certifications";
+import Courses from "./components/courses";
+import Roadmaps from "./components/roadmaps";
+import Videos from "./components/videos";
+import Login from "./components/login";
+import Signup from "./components/signup";
+import Home from "./components/home";
+import About from "./components/about";
+import Logo from "./image/logo.jpg";
+import "./App.css";
+>>>>>>> 6574603 (Updated the viewchanges)
 
+import Coursedetail from './components/Coursedetail';
+import Course from './components/Course';
+import "bootstrap/dist/css/bootstrap.min.css";
+import CourseView from './components/CourseView.Js';
 function App() {
   return (
     <div>
       <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/course" element={<Course />} />
+        <Route exact path="/detail/:id" element={<Coursedetail />} />
+        <Route exact path="/view/" element={<CourseView />} />
+        <Route exact path="/logo.jpg" element={<Logo />} />
+        <Route exact path="/courses" element={<Courses />} />
+        <Route exact path="/certifications" element={<Certifications />} />
+        <Route exact path="/roadmaps" element={<Roadmaps />} />
+        <Route exact path="/videos" element={<Videos />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/signup" element={<Signup />} />
+      </Routes>
       <Footer />
     </div>
   );
 }
 
-export default App;*/
-/*
-
-import React from 'react';
-import './App.css'
-import { BrowserRouter, Routes, Route} from "react-router-dom";
-import Certifications from './component/certifications';
-import Courses from './component/courses';
-import Roadmaps from './component/roadmaps';
-import Videos from './component/videos';
-import Login from './component/login';
-import Signup from './component/signup';
-import Home from './component/home';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div>
-      <header style={{ backgroundColor: 'indigo', borderRadius: '0px', padding: '20px', height: '90px', display: 'flex', alignItems: 'center' }}>
-        <a href="/home" className="nav-link" style={{ margin: '0 auto', color: 'white', textDecoration: 'none', display: 'flex', alignItems: 'center' }}>
-          <img src="/path/to/your/image.jpg" alt="" style={{ width: '40px', height: '40px', marginRight: '5px' }} />
-          <span style={{ fontWeight: 'bold' }}>G4 Things</span>
-        </a>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '20px', marginLeft: 'auto', marginRight: 'auto' }}>
-          <a href="/courses" className="nav-link">Courses</a>
-          <a href="/certifications" className="nav-link">Certifications</a>
-          <a href="/roadmaps" className="nav-link">Roadmaps</a>
-          <a href="/videos" className="nav-link">Videos</a>
-        </div>
-        <div style={{ display: 'flex', alignItems: 'center', gap: '10px', marginLeft: 'auto', marginRight: 'auto' }}>
-          <a href="/login" className="btn-login">Login</a>
-          <a href="/signup" className="btn-signup">Signup</a>
-        </div>
-      </header>
-      </div>
-      <Routes>
-        <Route exact path="/courses" element={<Courses />} />
-        <Route exact path="/certifications" element={<Certifications />} />
-        <Route exact path="/roadmaps" element={<Roadmaps />} />
-        <Route exact path="/videos" element={<Videos />} />
-        <Route exact path="/home" element={<Home />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-
 export default App;
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- group4/src/index.js
+++ group4/src/index.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"></link>
 =======
 
 >>>>>>> b78665b (Done with header and footer navigation)
 const root = ReactDOM.createRoot(document.getElementById('root'));
+=======
+<link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"
+></link>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+>>>>>>> 6574603 (Updated the viewchanges)
 root.render(
   <React.StrictMode>
-   <Router>
-    <App />
-   </Router>
-
-    
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 );
 
