234b82020d4abd01854aa1c37e9b44ae57f1fd7e
Vidhya E N <envidhya99@gmail.com>
2023-05-03 00:34:36 +0530

    Courseview ui half completed

--- group4/src/App.js
+++ group4/src/App.js
@@ -81,7 +81,7 @@ import Footer from "./components/footer";
 import Header from "./components/header";
 import { Routes, Route } from "react-router-dom";
 import Certifications from "./components/certifications";
-import Courses from "./components/courses";
+
 import Roadmaps from "./components/roadmaps";
 import Videos from "./components/videos";
 import Login from "./components/login";
@@ -95,7 +95,7 @@ import "./App.css";
 import Coursedetail from './components/Coursedetail';
 import Course from './components/Course';
 import "bootstrap/dist/css/bootstrap.min.css";
-import CourseView from './components/CourseView.Js';
+import CourseView from './components/Courseview';
 function App() {
   return (
     <div>
@@ -106,9 +106,9 @@ function App() {
         <Route exact path="/about" element={<About />} />
         <Route exact path="/course" element={<Course />} />
         <Route exact path="/detail/:id" element={<Coursedetail />} />
-        <Route exact path="/view/" element={<CourseView />} />
+        <Route exact path="/view/:id" element={<CourseView />} />
         <Route exact path="/logo.jpg" element={<Logo />} />
-        <Route exact path="/courses" element={<Courses />} />
+        {/* <Route exact path="/courses" element={<Courses />} /> */}
         <Route exact path="/certifications" element={<Certifications />} />
         <Route exact path="/roadmaps" element={<Roadmaps />} />
         <Route exact path="/videos" element={<Videos />} />
